8fa2c764d6f0241775af730d781390051862adae
ding jiong <ding404@gmail.com>
2018-10-10 21:45:07 +0800

    1. get http server local address 2. set http server listen local address

--- node.js
+++ node.js
@@ -1,7 +1,34 @@
 var express = require('express');
+var http = require('http');
 var serveStatic = require('serve-static');
 
-var app = express()
-    .use(serveStatic(__dirname + '/public'))
-    .listen(3000);
-console.log('listening at http://10.70.2.246:3000');
+var app = express();
+var server = http.createServer(app);
+
+app.use(serveStatic(__dirname + '/public'));
+
+
+server.listen(3000, getLocalAddress());
+server.on('listening', function() {
+    console.log('Express server started on http://%s:%s'
+        , server.address().address
+        , server.address().port);
+});
+
+function getLocalAddress() {
+    var os = require('os');
+    var ifaces = os.networkInterfaces();
+    const localhost = 'localhost';
+    var address = localhost;
+    Object.keys(ifaces).some(function(ifname) {
+        ifaces[ifname].some(function(iface) {
+            if ('IPv4' === iface.family && iface.internal === false) {
+                console.log(ifname, iface.address);
+                address = iface.address;
+            }
+            return address !== localhost;
+        });
+        return address !== localhost;
+    });
+    return address;
+}

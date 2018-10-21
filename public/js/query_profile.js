9879a27232786b2ede5be03f16fd987c9e3e0df0
ding jiong <ding404@gmail.com>
2018-10-21 14:03:18 +0800

    1. use span instead of input for profile text

--- public/js/query_profile.js
+++ public/js/query_profile.js
@@ -4,8 +4,8 @@ function query_profile() {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var profile = JSON.parse(this.responseText);
-            document.getElementById("username").value = profile.username;
-            document.getElementById("email").value = profile.email;
+            document.getElementById("username").innerHTML = profile.username;
+            document.getElementById("email").innerHTML = profile.email;
         }
     };
     xhttp.open("GET", "/account/profile", true);
